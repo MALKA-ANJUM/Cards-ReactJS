86071e8ab5a000740a44f365c1b6409fc5130c1a
Malka <malkaanjum1999@gmail.com>
2022-08-13 16:18:35 +0530

    init

---
+++ src/Components/AddContact.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+class AddContact extends React.Component {
+  state = {
+    name: "",
+    email: "",
+  };
+  add = (e) => {
+    e.preventDefault();
+    if (this.state.name === "" && this.state.email === "") {
+      alert("All the fields are mandatory!");
+      return;
+    }
+    this.props.addContactHandler(this.state);
+    this.setState({ name: "", email: "" });
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <div className="ui main">
+        <h2>Add Contact</h2>
+        <form className="ui field" onSubmit={this.add}>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={this.state.name}
+            onChange={(e) => this.setState({ name: e.target.value })}
+          ></input>
+          <label>Email</label>
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={(e) => this.setState({ email: e.target.value })}
+          ></input>
+          <button className="ui button blue">Add</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default AddContact;
